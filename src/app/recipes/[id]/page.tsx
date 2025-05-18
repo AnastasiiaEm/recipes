4141d5801a6118ed4b51ed6984a25736118e8bb8
anastasiia-em-i <167425361+anastasiia-em-i@users.noreply.github.com>
2025-05-18 15:26:08 +0200

    Fixes

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -1,7 +1,11 @@
 import { Suspense } from 'react';
 import RecipeDetailsPage from './RecipePage';
 
-export default function RecipePageWrapper({ params }: { params: Promise<{ id: string }> }) {
+export default function RecipePageWrapper({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   return (
     <Suspense fallback={<p className="text-center my-10">Loading recipe...</p>}>
       <RecipeDetailsPage params={params} />
